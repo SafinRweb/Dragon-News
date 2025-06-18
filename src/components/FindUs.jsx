fd9f7d6fbd1627c1ab40f58a7e2111858559a715
SafinRweb <safinr.gg@gmail.com>
2025-06-18 11:25:12 +0600

    Stable Released

--- src/components/FindUs.jsx
+++ src/components/FindUs.jsx
@@ -3,13 +3,30 @@ import { FaFacebook, FaInstagram } from 'react-icons/fa';
 import { FaThreads, FaTwitch, FaX } from 'react-icons/fa6';
 
 const FindUs = () => {
+    const handleSocial=()=>{
+        document.getElementById("apply_modal").showModal();
+    }
     return (
-        <div>
+        <div className='mb-5'>
             <h2 className="font-semibold mb-3">Find Us On</h2>
             <div className="join flex join-vertical *:bg-base-100 *:py-6">
-                <button className="btn join-item justify-start"><FaFacebook/>Facebook</button>
-                <button className="btn join-item justify-start"><FaInstagram/>Instagram</button>
-                <button className="btn join-item justify-start"><FaThreads/>Threads</button>
+                <button onClick={() => handleSocial()} className="btn join-item justify-start"><FaFacebook />Facebook</button>
+                <button onClick={() => handleSocial()} className="btn join-item justify-start"><FaInstagram />Instagram</button>
+                <button onClick={() => handleSocial()} className="btn join-item justify-start"><FaThreads />Threads</button>
+                
+                <dialog id="apply_modal" className="modal">
+                    <div className="modal-box">
+                        <h3 className="font-bold text-lg">Social's</h3>
+                        <p className="py-4">
+                               Coming Soon!!!
+                        </p>
+                        <div className="modal-action">
+                            <form method="dialog">
+                                <button className="btn btn-neutral btn-sm">Close</button>
+                            </form>
+                        </div>
+                    </div>
+                </dialog>
             </div>
         </div>
     );
--- src/components/layout-component/RightNavbar.jsx
+++ src/components/layout-component/RightNavbar.jsx
@@ -1,10 +1,8 @@
-import SocialLogin from '../SocialLogin';
 import FindUs from '../FindUs';
 
 const RightNavbar = () => {
     return (
         <div className="space-y-5 mt-6 md:mt-0">
-            <SocialLogin />
             <FindUs />
         </div>
     );
